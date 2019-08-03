94d5918f83184f5caf5454da1999e8a0ca7b1df8
dor <dorzhey@mail.ru>
2019-08-03 00:07:57 -0400

    Changed the HTML for the site.

--- dorapp/views.py
+++ dorapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def tool_list(request):
-    return render(request, 'dorapp\\tool_list.html', {})
+    return render(request, 'dorapp/tool_list.html', {})
