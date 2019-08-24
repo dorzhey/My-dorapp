9df82384d44523106c779b7660a36313ad8eaea6
dor <dorzhey@mail.ru>
2019-08-24 16:11:18 +0800

    функция добавления записи

--- dorapp/urls.py
+++ dorapp/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.tool_list, name='tool_list'),
     path('tool/<int:pk>/', views.tool_detail, name='tool_detail'),
+    path('tool/new/', views.tool_new, name='tool_new'),
 ]
--- dorapp/views.py
+++ dorapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Tool
+from .forms import ToolForm
+from django.shortcuts import redirect
+from django.utils import timezone
+from django import forms
 
 # Create your views here.
 def tool_list(request):
@@ -9,3 +13,16 @@ def tool_list(request):
 def tool_detail(request, pk):
     tool = get_object_or_404(Tool, pk=pk)
     return render(request, 'dorapp/tool_detail.html', {'tool': tool})
+
+def tool_new(request):
+    if request.method == "POST":
+        form = ToolForm(request.POST)
+        if form.is_valid():
+            tool = form.save(commit=False)
+            tool.author = request.user
+            tool.stock_date = timezone.now()
+            tool.save()
+            return redirect('tool_detail', pk=tool.pk)
+    else:
+        form = ToolForm()
+    return render(request, 'dorapp/tool_edit.html', {'form': form})
